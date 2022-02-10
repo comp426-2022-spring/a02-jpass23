88b2f6330929d0c37c7f71e73c8d3896450c4754
jpass23 <jpass23@live.unc.edu>
2022-02-09 20:47:27 -0500

    Final Commit

--- flips.js
+++ flips.js
@@ -2,5 +2,6 @@ import minimist from 'minimist';
 import {coinFlips, countFlips} from "./modules/coin.mjs";
 const args = minimist(process.argv.slice(2));
 var x = coinFlips((isFinite(args['number']))?args['number']:1);
+//console.log(args['number']);
 console.log(x);
 console.log(countFlips(x));
