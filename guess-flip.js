c6a3f1a9831fc0dd9dfcfad439b29119599ec97b
jpass23 <jpass23@live.unc.edu>
2022-02-23 12:45:41 -0500

    Fixed error message

--- guess-flip.js
+++ guess-flip.js
@@ -2,4 +2,4 @@ import minimist from "minimist";
 import { flipACoin } from "./modules/coin.mjs";
 
 const args = minimist(process.argv.slice(2));
-console.log((args['call']!=null) ? flipACoin(args['call']) : "Error: No input.\nUsage: node guess-flip --call=[heads][tails]");
+console.log((args['call']=='heads') || (args['call']=='tails') ? flipACoin(args['call']) : "Error: No input.\nUsage: node guess-flip --call=[heads][tails]");
