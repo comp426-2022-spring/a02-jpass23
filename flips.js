ca0392172d7a7ad88435fcdfe6d5a8d301473191
jpass23 <jpass23@live.unc.edu>
2022-02-23 12:31:34 -0500

    Retry grader

--- flips.js
+++ flips.js
@@ -1,7 +1,7 @@
 import minimist from 'minimist';
 import {coinFlips, countFlips} from "./modules/coin.mjs";
 const args = minimist(process.argv.slice(2));
-var x = coinFlips((isFinite(args['number']))?args['number']:1);
+var y = coinFlips((isFinite(args['number']))?args['number']:1);
 //console.log(args['number']);
-console.log(x);
-console.log(countFlips(x));
+console.log(y);
+console.log(countFlips(y));
