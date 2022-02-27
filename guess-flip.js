a01aaba47f6035e3ea46246a103189e3a5114cfd
jpass23 <jpass23@live.unc.edu>
2022-02-27 15:45:43 -0500

    Bruh

--- guess-flip.js
+++ guess-flip.js
@@ -4,7 +4,7 @@ import { flipACoin } from "./modules/coin.mjs";
 const args = minimist(process.argv.slice(2));
 
 if (args['call'] == null){
-    console.log("Error: No input.")
+    console.log("Error: no input.")
     console.log("Usage: node guess-flip --call=[heads|tails]")
 }else if (args['call']=='heads' || args['call']=='tails'){
     console.log(flipACoin(args['call']))
